cad2b785512efbd4b11b80159e122735842cdb5d
adrianhajdin <adrianhajdin00@gmail.com>
2023-09-15 14:13:55 +0200

    fix skeleton color, improve metadata, change favicon

--- app/(root)/(home)/loading.tsx
+++ app/(root)/(home)/loading.tsx
@@ -5,15 +5,15 @@ const loading = () => {
   return (
     <main className="flex-center paddings mx-auto w-full max-w-screen-2xl flex-col">
       <section className="nav-padding w-full">
-        <Skeleton className="h-[274px] w-full rounded-lg bg-red-500" />
+        <Skeleton className="h-[274px] w-full rounded-lg bg-black-200/40" />
       </section>
 
       <section className="mt-6 flex w-full flex-col sm:mt-20">
-        <Skeleton className="h-10 w-56 bg-red-500" />
+        <Skeleton className="h-10 w-56 bg-black-200/40" />
         <div className="mt-12 flex w-full flex-wrap justify-center gap-16 sm:justify-start">
-          <Skeleton className="h-[440px] w-full sm:w-[356px] bg-red-500" />
-          <Skeleton className="h-[440px] w-full sm:w-[356px] bg-red-500" />
-          <Skeleton className="h-[440px] w-full sm:w-[356px] bg-red-500" />
+          <Skeleton className="h-[440px] w-full sm:w-[356px] bg-black-200/40" />
+          <Skeleton className="h-[440px] w-full sm:w-[356px] bg-black-200/40" />
+          <Skeleton className="h-[440px] w-full sm:w-[356px] bg-black-200/40" />
         </div>
       </section>
     </main>
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,15 @@ import type { Metadata } from 'next'
 export const metadata: Metadata = {
   title: 'JS Mastery',
   description: 'JS Mastery Resources',
+  other: {
+    'theme-color': '#0d1117',
+    "color-scheme": "dark only",
+    "twitter:image": 'https://i.ibb.co/d6TXxB2/homepage-thumbnail.jpg',
+    "twitter:card": "summary_large_image",
+    "og:url": "jsmastery.pro",
+    "og:image": 'https://i.ibb.co/d6TXxB2/homepage-thumbnail.jpg',
+    "og:type": "website",
+  }
 }
 
 export default function RootLayout({
